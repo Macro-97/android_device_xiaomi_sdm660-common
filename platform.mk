2f0d9b82828209bc6157929c127c034086b1902f
Nick-infinity <nikhilg.me@gmail.com>
2019-06-24 17:27:07 +0530

    changes for lavender
    Signed-off-by: Nick-infinity <nikhilg.me@gmail.com>

--- platform.mk
+++ platform.mk
@@ -27,7 +27,7 @@ PLATFORM_PATH := device/xiaomi/sdm660-common
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product-if-exists, build/target/product/embedded.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
 
@@ -39,9 +39,11 @@ DEVICE_PACKAGE_OVERLAYS := device/xiaomi/sdm660-common/overlay
 
 # Build Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="taimen-user 9 PQ3A.190605.003 5524043 release-keys"
+    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.3.5.0.PFGMIXM release-keys" \
+    PRODUCT_NAME="lavender" \
+    TARGET_DEVICE="lavender"
 
-BUILD_FINGERPRINT := google/taimen/taimen:9/PQ3A.190605.003/5524043:user/release-keys
+BUILD_FINGERPRINT := "xiaomi/lavender/lavender:9/PKQ1.180904.001/V10.3.5.0.PFGMIXM:user/release-keys"
 
 # Platform properties
 $(call inherit-product, $(PLATFORM_PATH)/platform_prop.mk)
